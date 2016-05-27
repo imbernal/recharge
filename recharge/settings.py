5f2c337f061f5bef37d68427ad6e320e674a9685
Israel Michel Bernal Martinez <imbernal9203@gmail.com>
2016-05-27 16:04:19 -0400

    fixes

--- recharge/settings.py
+++ recharge/settings.py
@@ -132,14 +132,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-# RESOURCES_DIR = '/media/shared/src/django-paypal-store-example/samplesite/resources/'
-PAYPAL_PDT_TOKEN = '*** PAYPAY PDT TOKEN ***'
-PAYPAL_EMAIL = 'imbernal9203@gmail.com'
-PAYPAL_RETURN_URL = 'http://127.0.0.1:8000'
+# RESOURCES_DIR = '/media/shared/src/django-paypal-store-example/samplesite/resources/''
 
 
-PAYPAL_URL = 'https://www.sandbox.paypal.com/au/cgi-bin/webscr'
-PAYPAL_PDT_URL = 'https://www.sandbox.paypal.com/au/cgi-bin/webscr'
+# PAYPAL_URL = 'https://www.sandbox.paypal.com/au/cgi-bin/webscr'
+# PAYPAL_PDT_URL = 'https://www.sandbox.paypal.com/au/cgi-bin/webscr'
+
+# PAYPAL_URL = 'https://www.paypal.com/au/cgi-bin/webscr'
+#PAYPAL_PDT_URL = 'https://www.paypal.com/au/cgi-bin/webscr'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -151,7 +151,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    '/static/',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
